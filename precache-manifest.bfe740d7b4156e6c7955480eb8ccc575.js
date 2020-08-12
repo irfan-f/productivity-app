bbecd14202f2c98e17175fff3b2d127b547ed465
irfan-f <irfan.filipovic23@gmail.com>
2020-08-12 14:40:01 -0700

    Updates

---
+++ precache-manifest.bfe740d7b4156e6c7955480eb8ccc575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeec6a9bbc54d3f1dbbe573557eb82a3",
+    "url": "/productivity-app/index.html"
+  },
+  {
+    "revision": "bfb4b19bb24658165a56",
+    "url": "/productivity-app/static/css/main.5918eaed.chunk.css"
+  },
+  {
+    "revision": "1f62a18c576923b124e6",
+    "url": "/productivity-app/static/js/2.52f69800.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/productivity-app/static/js/2.52f69800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb4b19bb24658165a56",
+    "url": "/productivity-app/static/js/main.3ebc1aa5.chunk.js"
+  },
+  {
+    "revision": "9d5682cdcfbc1cf8d7a5",
+    "url": "/productivity-app/static/js/runtime-main.05190680.js"
+  }
+]);
